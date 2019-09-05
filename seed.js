636c14ad0da5e2a97fe13faf6fbc62d4ca05df64
John H <johnnyhill13@gmail.com>
2019-09-05 08:19:49 -0700

    began creating a seed file for later

---
+++ seed.js
@@ -0,0 +1,40 @@
+[{
+    "Question" : "What color would borccli be if it were on Jupiter?",
+    "Answer" : "Probably, green",
+    "Screwy" : ["Blue", "Plaid", "Rainbow", "None", "Who"]
+},
+{
+    "Question" : "Who is the fastest runner alive?",
+    "Answer" : "Usain Bolt"
+    "Screwy" : ["Barrack Obama", "Michael Jordan", "Tom Cruise", "Florence Griffith-Joyner", "Brent"]
+},
+{
+    "Question" : "",
+    "Answer" : "Usain Bolt"
+    "Screwy" : ["Barrack Obama", "Michael Jordan", "Tom Cruise", "Florence Griffith-Joyner", "Brent"]
+},
+{
+    "Question" : "Who is the fastest runner alive?",
+    "Answer" : "Usain Bolt"
+    "Screwy" : ["Barrack Obama", "Michael Jordan", "Tom Cruise", "Florence Griffith-Joyner", "Brent"]
+},
+{
+    "Question" : "Who is the fastest runner alive?",
+    "Answer" : "Usain Bolt"
+    "Screwy" : ["Barrack Obama", "Michael Jordan", "Tom Cruise", "Florence Griffith-Joyner", "Brent"]
+},
+{
+    "Question" : "Who is the fastest runner alive?",
+    "Answer" : "Usain Bolt"
+    "Screwy" : ["Barrack Obama", "Michael Jordan", "Tom Cruise", "Florence Griffith-Joyner", "Brent"]
+},
+{
+    "Question" : "Who is the fastest runner alive?",
+    "Answer" : "Usain Bolt"
+    "Screwy" : ["Barrack Obama", "Michael Jordan", "Tom Cruise", "Florence Griffith-Joyner", "Brent"]
+},
+{
+    "Question" : "Who is the fastest runner alive?",
+    "Answer" : "Usain Bolt"
+    "Screwy" : ["Barrack Obama", "Michael Jordan", "Tom Cruise", "Florence Griffith-Joyner", "Brent"]
+}]
