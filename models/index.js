125630cac4dccc139cb33854b974d4d5cd2dffe5
John H <johnnyhill13@gmail.com>
2019-09-10 22:54:23 -0700

    heroku test

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const DB_URL = process.env.DB_URI ||'mongodb://localhost:27017/jacket-pocket-backend';
+const DB_URL = process.env.MONGODB_URI ||'mongodb://localhost:27017/jacket-pocket-backend';
 
 mongoose.connect(DB_URL, {useNewUrlParser: true, useFindAndModify: false })
     .then(() => console.log('MongoDB connected...'))
