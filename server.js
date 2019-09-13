8ee6758ff869062e52622f1a0929177fb01ff39f
John H <johnnyhill13@gmail.com>
2019-09-13 13:27:35 -0700

    troubleshooting

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 const corsOptions = {
     // origin: ['http://localhost:3000'],
-    origin: ['https://jacket-pocket.herokuapp.com'],
+    origin: ['http://jacket-pocket.herokuapp.com'],
     credentials: false,
     optionsSuccessStatus: 200
   }
